d420cdd947616c950803fe10660339a9d6a67b25
zerkalica <zerkalica@users.noreply.github.com>
2020-03-08 07:07:58 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -3450,7 +3450,7 @@ var $;
         version() {
             return this.stat().mtime.getTime().toString(36).toUpperCase();
         }
-        exists(next) {
+        exists(next, force) {
             let exists = true;
             try {
                 this.stat();
@@ -3561,6 +3561,9 @@ var $;
     __decorate([
         $.$mol_mem
     ], $mol_file.prototype, "version", null);
+    __decorate([
+        $.$mol_mem
+    ], $mol_file.prototype, "exists", null);
     __decorate([
         $.$mol_mem
     ], $mol_file.prototype, "type", null);
